1420fc61e1d320fb479573d19a609d924c846a5c
xnomadbb <xnomadbb@gmail.com>
2014-11-20 13:10:15 -0600

    Add config loading from other folders

--- server/config.js
+++ server/config.js
@@ -1,11 +1,32 @@
 "use strict";
-var config;
+var fs = require('fs');
 
-//TODO Look in usual location for config folder, default to the following
-config = require(__dirname + '/../config/arrtorrent');
+function loadConfigFromFolder(configFolder) {
+	var config = require(configFolder + '/arrtorrent.js');
+	config.ssl_key = configFolder + '/server.key';
+	config.ssl_cert = configFolder + '/server.crt';
+	return config;
+}
 
-//TODO Create these paths from config folder
-config.ssl_key = __dirname + '/../config/server.key';
-config.ssl_cert = __dirname + '/../config/server.crt';
+function configFolderIsValid(configFolder) {
+	//TODO Should check more things to provide better feedback
+	return fs.existsSync(configFolder + '/arrtorrent.js');
+}
 
-module.exports = config;
+function locateConfigFolder() {
+	//TODO Do XDG config stuff instead/additionally
+	if ('HOME' in process.env) {
+		// Load config from home directory
+		var homeConfigFolder = process.env.home + '/.config/arrtorrent/';
+		if (configFolderIsValid(homeConfigFolder)) {
+			return homeConfigFolder;
+		}
+	}
+	var defaultFolder = __dirname + '/../config/';
+	if (configFolderIsValid(defaultFolder)) {
+		return defaultFolder;
+	}
+	throw new Error('No valid configuration found.');
+}
+
+module.exports = loadConfigFromFolder(locateConfigFolder());

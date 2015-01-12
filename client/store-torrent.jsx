86396981d6e73c0efd4e02481309f026facb57f8
xnomadbb <xnomadbb@gmail.com>
2015-01-11 21:24:19 -0600

    Redo torrent store, add view store

--- client/store-torrent.jsx
+++ client/store-torrent.jsx
@@ -1,19 +1,13 @@
 const EventEmitter = require('events').EventEmitter;
 const ArrRpc = require('./rpc');
+const Constants = require('./constants');
 
 class TorrentStore extends EventEmitter {
 	constructor() {
-		this.viewIds = [];
-		this.viewNames = {};
-		this.viewHashes = {};
-
-		this.builtinViewIds = ['main', 'default', 'name', 'active', 'started', 'stopped', 'complete', 'incomplete', 'hashing', 'seeding', 'leeching'];
-		this.blacklistViewIds = [
-			// Useless?
-			'default', 'name',
-			// rutorrent ratio groups (not sure)?
-			'rat_0', 'rat_1', 'rat_2', 'rat_3', 'rat_4', 'rat_5', 'rat_6', 'rat_7'
-		];
+		//this.builtinViewIds = ['main', 'default', 'name', 'active', 'started', 'stopped', 'complete', 'incomplete', 'hashing', 'seeding', 'leeching'];
+		// Useless?  'default', 'name',
+		// rutorrent ratio groups (not sure)?  'rat_0', 'rat_1', 'rat_2', 'rat_3', 'rat_4', 'rat_5', 'rat_6', 'rat_7'
+		this.torrents = {};
 
 		if (ArrRpc.config) {
 			this.loadInit();
@@ -23,40 +17,83 @@ class TorrentStore extends EventEmitter {
 	}
 
 	loadInit() {
-		ArrRpc.sendRequest('view_list', [], response => {
-			this.viewIds = [];
-			// Determine names from ids
-			for (let i=0; i < response.result.length; i++) {
-				let id = response.result[i];
-				if (this.blacklistViewIds.indexOf(id) === -1) {
-					this.viewIds.push(id);
-					this.viewNames[id] = this.nameFromId(id);
-				}
-			}
-			this.updateViewHashes(this.viewIds);
-		});
+		//TODO cache torrents in localstorage so we can start displaying immediately?
+
+		// Load all torrent data
+		let fieldList = [].concat(Constants.torrent.commands.immutable, Constants.torrent.commands.mutable, Constants.torrent.commands.dynamic);
+		this.queryTorrentInfo('main', fieldList);
 	}
 
-	updateViewHashes(viewList) {
-		let multicalls = viewList.map(v => ['d.multicall', [v, 'd.get_hash=']]);
-		ArrRpc.sendRequest('arr.multicall', multicalls, response => {
-			// Associate infohash list with each view
-			for (let i=0; i < response.result.length; i++) {
-				this.viewHashes[viewList[i]] = response.result[i].map(r => r[0]) || [];
-			}
-			console.log(viewList, this.viewHashes);
-			this.emit('view.change');
-		});
-	}
-
-	nameFromId(viewId) {
-		// Just capitalize builtin names
-		if (this.builtinViewIds.indexOf(viewId) !== -1) {
-			return viewId[0].toUpperCase() + viewId.slice(1);
+	// Fetch given fields of given torrents
+	queryTorrentInfo(view, fieldList) {
+		// Format multicall
+		let multicall_args = [view];
+		for (let i=0; i < fieldList.length; i++) {
+			multicall_args.push(fieldList[i] + '=');
 		}
 
-		//TODO Parse ids for fancy arr-defined views (per-tracker, label, etc)
-		return 'TODO Non-builtin view id specified';
+		ArrRpc.sendRequest('d.multicall', multicall_args, response => {
+			// Save results, remove everything not listed if we queried all torrents (main)
+			let removeUnlisted = (view === 'main');
+			this.mergeTorrentInfo(response.result, fieldList, removeUnlisted);
+		});
+	}
+
+	// Merge given torrent info into store and emit change events
+	mergeTorrentInfo(infoList, fieldList, removeUnlisted=false) {
+		let hashIndex = fieldList.indexOf('d.get_hash');
+		if (hashIndex === -1) {
+			console.error('Cannot find hash in torrent fieldList', infoList, fieldList); //XXX logging
+		}
+
+		// {hash: torrent} of changes
+		let changes = {
+			added: {},
+			removed: {},
+			modified: {},
+		};
+
+		// Merge info for each torrent
+		for (let i=0; i < infoList.length; i++) {
+			// Prepare source and dest locations
+			let srcInfo = infoList[i];
+			let hash = srcInfo[hashIndex];
+			let dstInfo = this.torrents[hash];
+			if (dstInfo === undefined) {
+				// Save reference to new object
+				// Note that modified will contain everything in added
+				dstInfo = changes.added[hash] = this.torrents[hash] = {};
+			}
+
+			// Copy each source field over
+			let isModified = false;
+			for (let j=0; j < fieldList.length; j++) {
+				let fieldName = Constants.torrent.commandToField[fieldList[j]];
+				if (dstInfo[fieldName] !== srcInfo[j]) {
+					dstInfo[fieldName] = srcInfo[j];
+					isModified = true;
+				}
+			}
+			if (isModified) {
+				changes.modified[hash] = dstInfo;
+			}
+		}
+
+		if (removeUnlisted) {
+			// Remove anything not in infoList, as infoList contains everything
+			let oldHashes = Object.keys(this.torrents);
+			let newHashes = new Set([for (t of infoList) t[hashIndex]]);
+			// ES6 has sets but no proper set operations ~.~
+			for (let i=0; i < oldHashes.length; i++) {
+				if (!newHashes.has(oldHashes[i])) {
+					changes.removed[oldHashes[i]] = this.torrents[oldHashes[i]];
+					delete this.torrents[oldHashes[i]];
+				}
+			}
+		}
+
+		// Tell the world
+		this.emit('change', changes);
 	}
 }
 
---
+++ client/store-views.jsx
@@ -0,0 +1,173 @@
+const EventEmitter = require('events').EventEmitter;
+const sha1 = require('sha1');
+const ArrRpc = require('./rpc');
+const TorrentStore = require('./store-torrent');
+
+// There are currently views for states, labels, and trackers. rutorrent does searches and feeds as well.
+class ViewStore extends EventEmitter {
+	constructor() {
+		this.viewGroups = {
+			// {group id: [view id]}
+			'state': ['state_all', 'state_downloading', 'state_complete', 'state_active', 'state_inactive', 'state_error'],
+			'label': ['label_none'],
+			'tracker': [],
+		};
+		this.groupNames = {
+			// {group id: group name}
+			'state': 'State',
+			'label': 'Label',
+			'tracker': 'Tracker',
+		};
+		this.viewNames = {
+			// {view id: view name}
+			'state_all': 'All',
+			'state_downloading': 'Downloading',
+			'state_complete': 'Complete',
+			'state_active': 'Active',
+			'state_inactive': 'Inactive',
+			'state_error': 'Error',
+			'label_none': 'None',
+		};
+		this.viewContents = {
+			// {view id: {hash: torrent}}
+			// These are all references, shouldn't be too bad on memory
+			'state_all': {},
+			'state_downloading': {},
+			'state_complete': {},
+			'state_active': {},
+			'state_inactive': {},
+			'state_error': {},
+			'label_none': {},
+		};
+		this.viewRtorrentViews = {
+			// {view id: rtorrent view name} when applicable
+			// We can perform operation on rtorrent-defined views en masse with d.multicall
+			// Generally these are only state views, we'd have to maintain other views manually
+			//TODO check all of these, correlate with _addTorrentToViews below
+			'state_all': 'main',
+			'state_downloading': 'leeching',
+			'state_complete': 'complete',
+			'state_active': 'active',
+			//'state_inactive': does this exist?
+			//'state_error': does this exist?
+			//this.builtinViewIds = ['main', 'default', 'name', 'active', 'started', 'stopped', 'complete', 'incomplete', 'hashing', 'seeding', 'leeching'];
+			// Useless?  'default', 'name',
+			// rutorrent ratio groups (not sure)?  'rat_0', 'rat_1', 'rat_2', 'rat_3', 'rat_4', 'rat_5', 'rat_6', 'rat_7'
+		};
+
+		if (ArrRpc.config) {
+			this.loadInit();
+		} else {
+			ArrRpc.once('configLoaded', this.loadInit.bind(this));
+		}
+
+		TorrentStore.on('change', this.torrentsDidChange.bind(this));
+	}
+
+	loadInit() {
+		//TODO load trackers
+	}
+
+	torrentsDidChange(changes) {
+		// Remove records for removed and modified torrents (we'll re-add modified)
+		for (let i=0; i < this.viewContents.length; i++) {
+			let viewTorrents = this.viewContents[i];
+			for (let hash in changes.removed) {
+				delete viewTorrents[hash];
+			}
+			for (let hash in changes.modified) {
+				delete viewTorrents[hash];
+			}
+		}
+
+		// Re-add modified torrents
+		for (let hash in changes.modified) {
+			this._addTorrentToViews(changes.modified[hash]);
+		}
+
+		// Add new torrents
+		for (let hash in changes.added) {
+			this._addTorrentToViews(changes.added[hash]);
+		}
+
+		this._pruneEmptyViews();
+		this.emit('change');
+	}
+
+	_addTorrentToViews(torrent) {
+		// Process user-defined label views
+		let anyLabels = false;
+		for (let i=1; i < 6; i++) {
+			let input = torrent['custom' + i];
+			if (input.length === 0) {
+				continue;
+			}
+			let label = 'label_' + sha1(input); // CSS-safe unique id
+			this._addView(label, input, 'label'); // Ensure view exists
+			this.viewContents[label][torrent.hash] = torrent;
+			anyLabels = true;
+		}
+
+		if (!anyLabels) {
+			// Add to no labels
+			this.viewContents.label_none[torrent.hash] = torrent;
+		}
+
+		// Process state views
+		//TODO Check that these are correct and that they correlate 1:1 with builtin view names, they
+		//     need to be exactly equivalent or we risk affecting unwanted torrents on batch operations
+		if (torrent.is_complete === '1') {
+			this.viewContents.state_complete[torrent.hash] = torrent;
+		} else {
+			this.viewContents.state_downloading[torrent.hash] = torrent;
+		}
+		if (torrent.is_active === '1') {
+			this.viewContents.state_active[torrent.hash] = torrent;
+		} else {
+			this.viewContents.state_inactive[torrent.hash] = torrent;
+		}
+		if (torrent.is_hashing_failed === '1' || torrent.message !== '') {
+			this.viewContents.state_error[torrent.hash] = torrent;
+		}
+
+		//TODO Process tracker views
+	}
+
+	// Add view to store if it doesn't yet exist
+	_addView(viewId, viewName, groupId) {
+		if (viewId in this.viewContents) {
+			// Already exists
+			return;
+		}
+
+		this.viewGroups[groupId].push(viewId); //TODO order by viewName or ?
+		this.viewNames[viewId] = viewName;
+		this.viewContents[viewId] = {};
+	}
+
+	_pruneEmptyViews() {
+		for (let viewId in this.viewContents) {
+			if (viewId.indexOf('state_') === 0) {
+				continue; // State views are rt-builtin
+			}
+			if (viewId === 'label_none') {
+				continue; // No labels is arr-builtin
+			}
+			if (Object.keys(this.viewContents[viewId]).length > 0) {
+				continue; // View not empty
+			}
+
+			// View is empty and prunable
+			delete this.viewContents[viewId];
+			delete this.viewRtorrentViews[viewId];
+			delete this.viewNames[viewId];
+
+			// Remove from viewGroups
+			let groupId = viewId.split('_')[0];
+			let viewIndex = this.viewGroups[groupId].indexOf(viewId);
+			this.viewGroups[groupId].splice(viewIndex, 1);
+		}
+	}
+}
+
+module.exports = new ViewStore();
